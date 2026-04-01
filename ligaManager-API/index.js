78cff34426b98e69bcb7f79f4539ef74bcff57ed
SoriannysAvilez <blue.diamond.escritora@gmail.com>
2026-04-01 18:57:34 -0500

    codigo index-backend

--- ligaManager-API/index.js
+++ ligaManager-API/index.js
@@ -1,7 +1,9 @@
-const app = require('./src/app');
+require('dotenv').config()
 
-const PORT = 3000;
+const app = require('./app')
+
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
-});
+    console.log(`API Liga de Manager corriendo en: http:??localhost: ${PORT}`)
+})
